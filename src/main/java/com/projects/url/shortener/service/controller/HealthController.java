3b6fcf96f8e7f65343d59bb97ef7278b8dcebae2
vinayprasanarao <vinayrao4343@gmail.com>
2026-09-14 18:06:22 -0500

    1. Updated project structure 2. Added application yml files

--- src/main/java/com/projects/url/shortener/service/controller/HealthController.java
+++ src/main/java/com/projects/url/shortener/service/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.projects.url.shortener.service;
+package com.projects.url.shortener.service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;

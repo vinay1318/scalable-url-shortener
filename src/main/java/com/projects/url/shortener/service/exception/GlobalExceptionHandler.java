5796e7614b01601cc6c94cc02344ceafa769280a
vinayprasanarao <vinayrao4343@gmail.com>
2026-09-18 18:56:46 -0500

    Added Global Exception Handler

---
+++ src/main/java/com/projects/url/shortener/service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.projects.url.shortener.service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String,Object>> handleGenericException(Exception exception){
+
+        Map<String,Object> response = new HashMap<>();
+
+        response.put("status",HttpStatus.INTERNAL_SERVER_ERROR.value());
+        response.put("error","Internal Server Error");
+        response.put("message",exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
+
+
+
+
+    }
+}
